c63ba2e76d72eec02084b7efa666f2c341d1be0c
Khanh Minh <khanhminh.work@gmail.com>
2024-11-26 13:35:41 +0700

    minh updated

--- src/main/java/org/example/sunsetresortwebapp/Controllers/UserController.java
+++ src/main/java/org/example/sunsetresortwebapp/Controllers/UserController.java
@@ -79,11 +79,6 @@ public class UserController {
                 }
         }
 
-//        @PostMapping("/updateprofile")
-//        public String processUpdateProfile(@RequestParam String fullname ,@RequestParam String email, @RequestParam String phonenumber, @RequestParam String address, Model model, HttpSession session){
-//
-//        }
-
         @PostMapping("/signup")
         public String processSignUp(@RequestParam String email, @RequestParam String password, @RequestParam  String confirmPassword, @RequestParam  String fullname, @RequestParam  String  address, @RequestParam String phoneNumber , Model model){
                 if(!confirmPassword.equalsIgnoreCase(password)){
